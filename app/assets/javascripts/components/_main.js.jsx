dfce865cf8782a3a677b3b5d8f4f575a6324d28d
yoga permana tanaya <tanayayoga@gmail.com>
2018-11-05 23:37:08 +0800

    rapiin ui dikit

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -1,7 +1,7 @@
 const Main=(props)=>{
     return(
         <div className='container'>
-            <div className='row'>
+            <div className='row' style={{display: 'flex', justifyContent: 'center'}}>
                 <h1>Fruits Inventory</h1>
                 <Body/>
             </div>
